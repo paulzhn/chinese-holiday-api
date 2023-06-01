1cd52e138eef5fb4574fbcff3cbc01a9dd748802
Paul Zhang <me@paulzhn.me>
2023-06-01 15:19:09 +0000

    fix: main.go import package
    Signed-off-by: Paul Zhang <me@paulzhn.me>

--- api/holiday.go
+++ api/holiday.go
@@ -1,4 +1,4 @@
-package holiday
+package api
 
 import (
 	"encoding/json"
--- main.go
+++ main.go
@@ -8,11 +8,9 @@ import (
 
 func main() {
 	// register http handler
-	// here's code
 	http.HandleFunc("/api/holiday", api.Handler)
 
 	// start http server
-	// here's code
 	http.ListenAndServe(":8080", nil)
 
 }
